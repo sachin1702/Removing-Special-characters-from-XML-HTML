b2f8207fb84ba47bf0bf0ba5ee34978ce7a30a3f
Cue_the_music <57125562+sachin1702@users.noreply.github.com>
2020-06-25 13:27:45 +0530

    Update DWH-BSCS.py

--- DWH-BSCS.py
+++ DWH-BSCS.py
@@ -1,23 +1,29 @@
-#!/usr/bin/env python
-# coding: utf-8
+import os,glob
+import re
+import paramiko
+import cx_Oracle 
 
-# In[ ]:
+folder_path = r'C:\Users\Desktop\Project Python\BSCS-DWH'
 
+for filename in glob.glob(os.path.join(folder_path, '*.xml')):
+  #with open(filename, 'r') as f:
+    # Open the file and read to generate Information in string format
+    
+    
+    # 0020 — 007F   Basic Latin
+    # 00A0 — 00FF   Latin-1 Supplement
+    # 0100 — 017F   Latin Extended-A
+    # 0180 — 024F   Latin Extended-B
+    # \u0621-\u064A0-9 Arabic Characters
+   
 
-inFile = open(r'C:\Users\ENQRRUH\Desktop\Project Python\BSCS-DWH\ADDR23583409.889400.xml', 'r',encoding='utf-8').read()
+    result = re.sub(r'[^\u0020-\u007F\u00A0-\u00FF\u0100-\u017F\u0180-\u024F\u0621-\u064A0-9]',r'', inFile)
+    print(result)
 
-# prints "123" by keeping all characters from the following ranges:
-# 0020 — 007F   Basic Latin
-# 00A0 — 00FF   Latin-1 Supplement
-# 0100 — 017F   Latin Extended-A
-# 0180 — 024F   Latin Extended-B
-# \u0621-\u064A0-9 Arabic Characters
-
-result = re.sub(r'[^\u0020-\u007F\u0621-\u064A0-9\u00A0-\u00FF\u0100-\u017F\u0180-\u024F]',r'', inFile)
-print(result)
-
-# generating an outputfile
-outFile = open(r'C:\Users\ENQRRUH\Desktop\Project Python\BSCS-DWH\ADDR23583409.8894001.xml', 'w',encoding='utf-8')
-outFile.writelines(result)
-outFile.close()
+    # generating an outputfile
+    outFile = open(filename, 'w',encoding='utf-8')
+    outFile.writelines(result)
+    outFile.close()
+    text = f.read()
+    print (filename)'''
 
